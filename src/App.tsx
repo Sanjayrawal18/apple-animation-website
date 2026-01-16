dcacf7d5cf9606f862bda192084e4f2bb46126e0
Sanjayrawal18 <rawalsanjay204@gmail.com>
2026-01-16 22:50:48 +0530

    Implement hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import type { JSX } from "react"
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 const App = (): JSX.Element => {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, type JSX } from "react";
+
+const Hero = (): JSX.Element => {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+        if (videoRef.current) videoRef.current.playbackRate = 2;
+    }, [])
+
+  return (
+    <section id="hero">
+      <div>
+        <h1>MacBook Pro</h1>
+        <img src="/title.png" alt="MacBook Title" />
+      </div>
+
+      <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline></video>
+
+      <button>Buy</button>
+
+      <p>From $1599 or $133/mo for 12 months</p>
+    </section>
+  )
+}
+
+export default Hero;
